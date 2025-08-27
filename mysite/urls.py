e9bd865f1e94b5f91c09af51b6f02cc0f78d7e86
NguyenTYNhi <2003yennhinguyenn@gmail.com>
2025-08-27 10:53:49 +0700

    Dong bo code

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("polls/", include("polls.urls")),  # include app polls
+    path("polls/", include("viewLayer.polls.urls")),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,42 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.urls import reverse
-from django.http import HttpResponseRedirect
+import datetime
 
-
-def index(request):
-    return render(request, "polls/index.html")
-
-
-def special_case_2003(request):
-    return HttpResponse("Special case: year 2003")
-
-
-def year_archive(request, year):
-    return HttpResponse(f"Year archive: {year}")
-
-
-def month_archive(request, year, month):
-    return HttpResponse(f"Month archive: {year}/{month}")
-
-
-def article_detail(request, year, month, slug):
-    return HttpResponse(f"Article: {year}/{month}, slug={slug}")
-
-
-def page_with_regex(request, page):
-    return HttpResponse(f"Page via regex: {page}")
-
-
-def reports(request):
-    return HttpResponse("Credit reports page")
-
-
-def charge(request):
-    return HttpResponse("Credit charge page")
-
-
-# Ví dụ dùng reverse trong view
-def go_to_2005(request):
-    url = reverse("polls:year-archive", args=(2005,))
-    return HttpResponseRedirect(url)
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = f"<html lang='en'><body>It is now {now}.</body></html>"
+    return HttpResponse(html)
